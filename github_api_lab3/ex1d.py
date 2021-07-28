77672c560fce37f2975e42ab7a583e846f2fb6d9
Sumudu Liyanage <62101884+sumuduliyanage@users.noreply.github.com>
2021-07-28 14:37:22 +0530

    Update ex1d.py

--- github_api_lab3/ex1d.py
+++ github_api_lab3/ex1d.py
@@ -4,7 +4,7 @@
 import requests
 
 with requests.Session() as session:
-    session.headers['Authorization'] = 'token ed77e01fbdcc9b54a596e7e366eba88c9cdc251f'#giving the authorization
+    session.headers['Authorization'] = 'token ed7ssssssdjjdjjdf'#giving the authorization
     response = session.get("https://api.github.com/user")#getting data ---token is used for authentication
     
 print(response.json())
